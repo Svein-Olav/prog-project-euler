6ed173eab2783911a406df6b9f641ef32d23c7de
Svein Olav Andersen <svan@fhi.no>
2025-12-16 20:24:07 +0000

    Oppdatert slik at isAPrime virker

--- oppgave3/main.go
+++ oppgave3/main.go
@@ -7,6 +7,18 @@ import "fmt"
 func main() {
 	
 	fmt.Println("Hello, World!")
+
+	// prime numbers : 2, 3, 5, 7, 11, 13, 17, 19
+
+	for i :=1; i <= 20; i += 1 {
+		isPrime := IsAPrime(i)
+		if isPrime {
+			fmt.Println("is prime",i)
+		} else {
+			fmt.Println("is not prime",i)
+		}
+    }
+
 }
 
 func FinnAllePrimtallOpptil(number int) []int {
@@ -23,26 +35,21 @@ func FinnAllePrimtallOpptil(number int) []int {
 }
 
 func IsAPrime(i int) bool {
-	returnValue := false
+	returnValue := true
 
-	if i == 2 {
-		returnValue = true;
-	 }
-
-	 /*
-	 if(i % 2 == 0 && i > 2) {
-		returnValue = false
-	 }
-	 */
-	 for j := 3; j <= i; j += 2 {
-		fmt.Printf("i = %v: j = %v \n",i,j)
+	//if i == 2 {
+	//	returnValue = true;
+	// }
+	 
+	 for j := 2; j < i; j += 1 {
+		//fmt.Printf("i = %v: j = %v \n",i,j)
 		if(i % j == 0) {
-			fmt.Printf("har ingen rest")
+			//fmt.Println("har ingen rest")
 			returnValue = false
 			break
 		} else {
 			returnValue = true
-			fmt.Printf("har rest")
+			//fmt.Println("har rest")
 		}
 	} 
     return returnValue
