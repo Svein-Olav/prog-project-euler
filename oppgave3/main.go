11e2f9734c2527f106816ebd98778dc2fe086f99
Svein Olav Andersen <svan@fhi.no>
2025-12-16 14:38:49 +0000

    middler tidig innsjekking

--- oppgave3/main.go
+++ oppgave3/main.go
@@ -9,13 +9,41 @@ func main() {
 	fmt.Println("Hello, World!")
 }
 
+func FinnAllePrimtallOpptil(number int) []int {
+	var primtallsFaktorer []int
+	
+	for i := 1; i <= number; i += 1 {
+		if IsAPrime(i) {
+			primtallsFaktorer = append(primtallsFaktorer, i)
+		}
+	}
+	
+	fmt.Println(primtallsFaktorer)
+	return primtallsFaktorer
+}
+
 func IsAPrime(i int) bool {
 	returnValue := false
 
 	if i == 2 {
-
 		returnValue = true;
 	 }
-	
+
+	 /*
+	 if(i % 2 == 0 && i > 2) {
+		returnValue = false
+	 }
+	 */
+	 for j := 3; j <= i; j += 2 {
+		fmt.Printf("i = %v: j = %v \n",i,j)
+		if(i % j == 0) {
+			fmt.Printf("har ingen rest")
+			returnValue = false
+			break
+		} else {
+			returnValue = true
+			fmt.Printf("har rest")
+		}
+	} 
     return returnValue
 }
--- oppgave3/main_test.go
+++ oppgave3/main_test.go
@@ -3,13 +3,43 @@ package main
 import "testing"
 
 func TestIsAPrime(t *testing.T) {
+	/*
 	result := IsAPrime(2)
-
 	expected := true
 
 	if result != expected {
 		t.Errorf("IsAPrime(2) = %v; vil ha %v", result, expected)
 
+	}
+*/
+	expected := false
+	if IsAPrime(17) != expected {
+		t.Errorf("IsAPrime(10) = %v; vil ha %v", IsAPrime(11), false)
+	}
+
+	/*
+	expected = true
+	if IsAPrime(1009) != true {
+		t.Errorf("IsAPrime(1009) = %v; vil ha %v", IsAPrime(1009), true)
+	}
+		*/
+
+}
+
+func TestFinnAllePrimtallOpptil(t *testing.T) {
+	result := FinnAllePrimtallOpptil(28)
+	expected := []int{2, 7}
+
+	if len(result) != len(expected) {
+		t.Errorf("FinnAllePrimtallOpptil(28) = %v; vil ha %v", result, expected)
+		return
+	}
+
+	for i := range result {
+		if result[i] != expected[i] {
+			t.Errorf("FinnAllePrimtallOpptil(28) = %v; vil ha %v", result, expected)
+			return
+		}
 	}
 
 }
