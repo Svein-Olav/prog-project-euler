06abadc2ed6199cb67a88cac863deb17d8113dc8
Svein Olav Andersen <svan@fhi.no>
2026-01-04 21:20:21 +0000

    Denne virker

--- oppgave3/main.go
+++ oppgave3/main.go
@@ -6,38 +6,49 @@ import "fmt"
 // lag en funksjon som sorterer en liste
 func main() {
 	
-	fmt.Println("Hello, World!")
-
-	// prime numbers : 2, 3, 5, 7, 11, 13, 17, 19
-
-	for i :=1; i <= 20; i += 1 {
-		isPrime := IsAPrime(i)
-		if isPrime {
-			fmt.Println("is prime",i)
-		} else {
-			fmt.Println("is not prime",i)
-		}
-    }
-
-	FindPrimFactorsUpTo(500)
-
+	fmt.Println("Hello ")
+	
+	gdc()
 }
 
 func gdc(){
-	primeFactors :=  FindPrimFactorsUpTo(500)
-	tall := 6000
 
-	for len(primeFactors) > 0 {
-		primeNumber := primeFactors[0]
+	const number = 600851475143
+	
+	primes := FindPrimFactorsUpTo(number)
+	primeFactors := []int{} 
+	tall := number
+
+	for len(primes) > 0 {
+		
+		primeNumber := primes[0]
 
 		if (tall % primeNumber == 0) {
+			tall = tall / primeNumber
+
+			primeFactors = append(primeFactors, primeNumber)
 			
+		} else {
+			primes = primes[1:]
 		}
-
-
 	}
 
+	fmt.Println("**************")
+	fmt.Println("List of primes factors", primeFactors)
+	sum := MultiplyFactors(primeFactors)
+	fmt.Println("Product of prims", sum )
 
+	fmt.Println("Larges factor", primeFactors[len(primeFactors)-1])
+}
+
+func MultiplyFactors(numbers []int) int{
+	product := 1
+
+	for i := 0; i < len(numbers); i += 1 {
+		product = product * numbers[i]
+	}
+
+	return product
 
 }
 
@@ -46,7 +57,8 @@ func gdc(){
 func FindPrimFactorsUpTo(number int) []int {
 	var primeFactors []int
 
-	for i := 1; i <= number; i += 1 {
+	for i := 1; i*i <= number; i += 1 {
+		fmt.Println("Numbers tested",i," . Primenumbers", len(primeFactors))
 		if IsAPrime(i) {
 			primeFactors = append(primeFactors, i)
 		}
@@ -62,7 +74,7 @@ func IsAPrime(i int) bool {
 
 	if (i == 1) { return false}
 		 
-	for j := 2; j < i; j += 1 {		
+	for j := 2; j*j < i; j += 1 {		
 		if(i % j == 0) {
 			// har ingen rest, altså ikke et primtall
 			returnValue = false
